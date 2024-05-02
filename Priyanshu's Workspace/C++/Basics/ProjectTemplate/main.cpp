1028b991f4fdd10cf28168750d215849959ace19
Priyanshu Sahu <priyanshusahu91@gmail.com>
2024-05-02 10:53:54 +0530

    Testing something

--- Priyanshu's Workspace/C++/Basics/ProjectTemplate/main.cpp
+++ Priyanshu's Workspace/C++/Basics/ProjectTemplate/main.cpp
@@ -4,6 +4,10 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    int arr[] = {0,1,2,3,4,5,6,7,8,9};
+    for ( auto i : arr ) //Range based loop.
+    {
+        std::cout << "value : " << i << std::endl;
+    }
     return 0;
 }
