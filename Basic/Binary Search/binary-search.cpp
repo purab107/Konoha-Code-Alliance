fd080fca35c2efd0ed02d2ee9f565f1515dbdc16
purab107 <159408131+purab107@users.noreply.github.com>
2024-05-07 22:24:35 +0530

    Squashed 'Pritesh's Workspace/GeeksForGeeksCodes/' content from commit bc36136
    git-subtree-dir: Pritesh's Workspace/GeeksForGeeksCodes
    git-subtree-split: bc36136c6da4dc7f2c32ed4ac877a8a73d88c246

---
+++ Basic/Binary Search/binary-search.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int left = 0, right = n - 1;
+    while (left <= right) {
+        if (arr[left] == k) return left;
+        if (arr[right] == k) return right;
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == k) return mid;
+        if (arr[mid] > k) right = mid - 1;
+        else left = mid + 1;
+    }
+    return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
